c307172ec1d61576b540f04c36c0fc866ee4d85a
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-03-26 21:08:55 +0900

    Main Page Prototype

---
+++ src/components/views/MainPage/Sections/NetflixSlider/Item.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import SliderContext from './context'
+import './Item.scss'
+
+function Item({ card }) {
+    // const handleHover = (e) => {
+    //     const data = {
+    //         // offsetHeight : e.currentTarget.parentElement.children[card.id-1].offsetHeight,
+    //         // offsetWidth : e.currentTarget.parentElement.children[card.id-1].offsetWidth,
+    //         // sliderOffsetWidth : e.currentTarget.parentElement.offsetWidth,
+    //         // sliderOffsetHeight : e.currentTarget.parentElement.offsetHeight,
+    //         // sliderOffsetX : e.currentTarget.parentElement.parentElement.parentElement.offsetLeft,
+    //         // sliderOffsetY : e.currentTarget.parentElement.parentElement.parentElement.offsetTop,
+    //         rect: e.currentTarget.parentElement.children[card.id-1].getBoundingClientRect(),
+    //         scrollY: window.scrollY
+    //     }
+    //     onItemHover(data)
+    // }
+    return (
+        <SliderContext.Consumer>
+            {({ elementRef }) => {
+                let isHovering = false;
+                return (
+                    <div
+                        ref={elementRef}
+                        className={'item'}
+                    >
+                        <a href={`/mapinfo/${card.id}`}>
+                            <img src={card.image} alt="" />
+                        </a>
+                    </div>
+                );
+            }}
+        </SliderContext.Consumer>
+    )
+}
+
+export default Item
---
+++ src/components/views/MainPage/Sections/NetflixSlider/modal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const modal = () => {
+    console.log("modal")
+    return (
+        <div style={{zIndex: '10'}}>
+            this is modal
+        </div>
+    )
+}
+
+export default modal
