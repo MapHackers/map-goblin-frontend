ceb3625d47ab2aa6cf31e94e0f8652676ebffaa2
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-03-26 17:17:41 +0900

    Initial Page design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React from 'react'
+
 import './App.css';
+import {
+  Switch,
+  Route,
+  BrowserRouter
+} from 'react-router-dom'
+
+import LandingPage from './components/views/LandingPage/LandingPage'
+import LoginPage from './components/views/LoginPage/LoginPage'
+import RegisterPage from './components/views/RegisterPage/RegisterPage'
+import FindIDPage from './components/views/FindIDPage/FindIDPage'
+import FindPasswordPage from './components/views/FindPasswordPage/FindPasswordPage'
+import MainPage from './components/views/MainPage/MainPage'
+import MyPage from './components/views/MyPage/MyPage'
+import { Suspense } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Suspense fallback={(<div> Loading ... </div>)}>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/register" component={RegisterPage} />
+            <Route exact path="/findId" component={FindIDPage} />
+            <Route exact path="/findPassword" component={FindPasswordPage} />
+            <Route exact path="/main" component={MainPage} />
+            <Route exact path="/:userId" component={MyPage} />
+          </Switch>
+        </Suspense>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/views/FindIDPage/FindIDPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FindIDPage() {
+    return (
+        <div>
+            FindIdPage
+        </div>
+    )
+}
+
+export default FindIDPage
