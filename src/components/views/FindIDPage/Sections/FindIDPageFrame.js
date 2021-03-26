253523f96755cfeb45f551f3b6396d2cd3a109e4
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-03-26 18:05:02 +0900

    FindID Page Prototype

---
+++ src/components/views/FindIDPage/Sections/FindIDPageFrame.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from './Logo'
+import Title from './Title'
+import FindIdForm from './FindIDForm'
+
+function FindIDPageFrame() {
+    return (
+        <div>
+            <Logo/>
+            <Title/>
+            <FindIdForm/>
+        </div>
+    )
+}
+
+export default FindIDPageFrame
---
+++ src/components/views/FindIDPage/Sections/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Title() {
+    return (
+        <div>
+            아이디 찾기
+        </div>
+    )
+}
+
+export default Title
