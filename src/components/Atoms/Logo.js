b279e54b01b951e0747f5d315543768e5c0872d7
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-03-27 21:03:27 +0900

    Landing Page Prototype

---
+++ src/components/Atoms/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LogoImage from '../../assets/images/Logo.png'
+
+function Logo(props) {
+    return (
+        <div>
+            <img style={{width:'40px', height:'40px'}} src={LogoImage} alt="Logo"/>
+        </div>
+    )
+}
+
+export default Logo
---
+++ src/components/views/LandingPage/Sections/LandingPageFrame.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from './NavBar'
+import MainImage from './MainImage'
+import Footer from './Footer'
+
+function LandingPageFrame() {
+    return (
+        <div>
+            <NavBar />
+            <MainImage />
+            <Footer />
+        </div>
+    )
+}
+
+export default LandingPageFrame
