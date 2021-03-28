4d47e80827abb3387d3df1e0634ffcf06582a62f
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-03-28 18:42:26 +0900

    Search Page ProtoType

--- src/components/views/NavigationBar/Sections/SearchByRepoName.js
+++ src/components/views/NavigationBar/Sections/SearchByRepoName.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Input } from 'antd'
+import { Redirect, Route, BrowserRouter, useHistory, withRouter, Link } from 'react-router-dom';
 
 const { Search } = Input;
-const onSearch = value => console.log(value);
 
-function SearchByRepoName() {
+function SearchByRepoName(props) {
+
+    const [searchValue, setsearchValue] = useState('')
+
+    const onChange = (event) => {
+        setsearchValue(event.currentTarget.value)
+    }
+
+    const onSearch = (value, e) => {
+        props.history.push(`/search?q=${value}`)
+    }
+
+
     return (
-        <div style={{ width: 600, height: 30, float: 'right', marginTop: '20px', marginRight: '100px', flex: 'none'}}>
-            <Search placeholder="레포지토리명으로 검색" onSearch={onSearch} size="large" />
+        <div style={{ width: 600, height: 30, float: 'right', marginTop: '20px', marginRight: '100px', flex: 'none' }}>
+            <Search placeholder="레포지토리명으로 검색" onSearch={onSearch} size="large" value={searchValue} onChange={onChange} />
         </div>
     )
 }
 
-export default SearchByRepoName
+export default withRouter(SearchByRepoName)
