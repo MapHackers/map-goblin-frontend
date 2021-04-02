7b6ba1547309a50014f6790206e011b7e5a0cf62
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-04-02 19:48:51 +0900

    Redux 사용을 위한 index.js 수정

--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider, } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise'
+import ReduxThunk from 'redux-thunk'
+import Reducer from './redux/_reducers'
+import { BrowserRouter } from 'react-router-dom';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider
+    store={createStoreWithMiddleware(Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+
+  , document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
