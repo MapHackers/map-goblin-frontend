cae23e47ca756a628ec24a03ef091607f08363dd
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-05-04 01:32:58 +0900

    findpassword by email code

--- src/components/Form/FindPasswordForm.js
+++ src/components/Form/FindPasswordForm.js
@@ -8,8 +8,11 @@ import { withRouter } from 'react-router-dom';
 
 function FindPasswordForm(props) {
     const [typePassword, settypePassword] = useState(false)
+    const [typeEmailCode, settypeEmailCode] = useState(false)
     const [userId, setuserId] = useState('')
     const [userToken, setuserToken] = useState('')
+    const [userEmail, setuserEmail] = useState('')
+
     return (
         <div>
             {!typePassword ?
@@ -33,12 +36,24 @@ function FindPasswordForm(props) {
                                 email: values.email
                             }
 
+                            let dataToSubmitEmail = {
+                                email: values.email
+                            }
+
                             axios.post(`/api/find`, dataToSubmit)
                                 .then(response => {
                                     setuserId(response.data.id)
                                     setuserToken(response.data.token)
-                                    alert("변경할 비밀번호를 입력해 주세요!")
-                                    settypePassword(true)
+                                    setuserEmail(response.data.email)
+                                    axios.post(`/api/email`, dataToSubmitEmail)
+                                        .then(response => {
+                                            alert("가입한 이메일을 확인하여 코드를 입력해 주세요.")
+                                            settypePassword(true)
+                                            settypeEmailCode(true)
+                                        })
+                                        .catch(err => {
+                                            alert("이메일 오류!")
+                                        })
                                 })
                                 .catch(err => {
                                     alert("해당 이메일로 만든 아이디가 없습니다!")
@@ -108,103 +123,180 @@ function FindPasswordForm(props) {
                     }
                 </Formik>
 
-                :
+                : (!typeEmailCode ?
+                    <Formik
+                        initialValues={{
+                            password: ''
+                        }}
+                        validationSchema={Yup.object().shape({
+                            password: Yup.string()
+                                .required('비밀번호를 입력 해주세요.'),
+                            confirmPassword: Yup.string()
+                                .oneOf([Yup.ref('password'), null], '비밀번호가 다릅니다.')
+                                .required('비밀번호 확인은 필수 항목입니다.')
+                        })}
+                        onSubmit={(values, { setSubmitting }) => {
+                            setTimeout(() => {
 
-                <Formik
-                    initialValues={{
-                        password: ''
-                    }}
-                    validationSchema={Yup.object().shape({
-                        password: Yup.string()
-                            .required('비밀번호를 입력 해주세요.'),
-                        confirmPassword: Yup.string()
-                            .oneOf([Yup.ref('password'), null], '비밀번호가 다릅니다.')
-                            .required('비밀번호 확인은 필수 항목입니다.')
-                    })}
-                    onSubmit={(values, { setSubmitting }) => {
-                        setTimeout(() => {
-
-                            let dataToSubmit = {
-                                password: values.password,
-                                confirmPassword: values.confirmPassword
-                            }
-
-                            axios.post(`/api/members/${userId}/password`, dataToSubmit, {
-                                headers: {
-                                    'Content-Type': 'application/json',
-                                    'X-AUTH-TOKEN': `${userToken}`,
+                                let dataToSubmit = {
+                                    password: values.password,
+                                    confirmPassword: values.confirmPassword
                                 }
-                            })
-                                .then(response => {
-                                    if (response.status === 200) {
-                                        alert("비밀번호 변경이 완료되었습니다. 로그인 해주세요")
-                                    }
-                                    props.history.push('/login')
-                                })
-                                .catch(err => {
-                                    console.log(err)
-                                })
 
-                            setSubmitting(false);
-                        }, 400);
-                    }}
-                >
-                    {props => {
-                        const {
-                            values,
-                            touched,
-                            errors,
-                            isSubmitting,
-                            handleChange,
-                            handleBlur,
-                            handleSubmit,
-                        } = props
-                        return (
-                            <form onSubmit={handleSubmit}>
-                                <Form.Item required>
-                                    <Input
-                                        id='password'
-                                        placeholder="비밀번호"
-                                        type='password'
-                                        value={values.password}
-                                        onChange={handleChange}
-                                        onBlur={handleBlur}
-                                        className={
-                                            errors.password && touched.password ? 'text-input error' : 'text-input'
+                                axios.post(`/api/members/${userId}/password`, dataToSubmit, {
+                                    headers: {
+                                        'Content-Type': 'application/json',
+                                        'X-AUTH-TOKEN': `${userToken}`,
+                                    }
+                                })
+                                    .then(response => {
+                                        if (response.status === 200) {
+                                            alert("비밀번호 변경이 완료되었습니다. 로그인 해주세요")
                                         }
-                                        size="large"
-                                    />
-                                    {errors.password && touched.password && (
-                                        <div className="input-feedback">{errors.password}</div>
-                                    )}
-                                </Form.Item>
-                                <Form.Item required>
-                                    <Input
-                                        id="confirmPassword"
-                                        placeholder="비밀번호 확인"
-                                        type="password"
-                                        value={values.confirmPassword}
-                                        onChange={handleChange}
-                                        onBlur={handleBlur}
-                                        className={
-                                            errors.confirmPassword && touched.confirmPassword ? 'text-input error' : 'text-input'
-                                        }
-                                        size="large"
-                                    />
-                                    {errors.confirmPassword && touched.confirmPassword && (
-                                        <div className="input-feedback">{errors.confirmPassword}</div>
-                                    )}
-                                </Form.Item>
-                                <Form.Item>
-                                    <Button type="primary" htmlType="submit" size="large" className="login-form-button" style={{ minWidth: '100%' }} disabled={isSubmitting} onSubmit={handleSubmit}>
-                                        비밀번호변경하기
+                                        props.history.push('/login')
+                                    })
+                                    .catch(err => {
+                                        console.log(err)
+                                    })
+
+                                setSubmitting(false);
+                            }, 400);
+                        }}
+                    >
+                        {props => {
+                            const {
+                                values,
+                                touched,
+                                errors,
+                                isSubmitting,
+                                handleChange,
+                                handleBlur,
+                                handleSubmit,
+                            } = props
+                            return (
+                                <form onSubmit={handleSubmit}>
+                                    <Form.Item required>
+                                        <Input
+                                            id='password'
+                                            placeholder="비밀번호"
+                                            type='password'
+                                            value={values.password}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            className={
+                                                errors.password && touched.password ? 'text-input error' : 'text-input'
+                                            }
+                                            size="large"
+                                        />
+                                        {errors.password && touched.password && (
+                                            <div className="input-feedback">{errors.password}</div>
+                                        )}
+                                    </Form.Item>
+                                    <Form.Item required>
+                                        <Input
+                                            id="confirmPassword"
+                                            placeholder="비밀번호 확인"
+                                            type="password"
+                                            value={values.confirmPassword}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            className={
+                                                errors.confirmPassword && touched.confirmPassword ? 'text-input error' : 'text-input'
+                                            }
+                                            size="large"
+                                        />
+                                        {errors.confirmPassword && touched.confirmPassword && (
+                                            <div className="input-feedback">{errors.confirmPassword}</div>
+                                        )}
+                                    </Form.Item>
+                                    <Form.Item>
+                                        <Button type="primary" htmlType="submit" size="large" className="login-form-button" style={{ minWidth: '100%' }} disabled={isSubmitting} onSubmit={handleSubmit}>
+                                            비밀번호변경하기
                                     </Button>
-                                </Form.Item>
-                            </form>
-                        )
-                    }
-                    }
-                </Formik>}
+                                    </Form.Item>
+                                </form>
+                            )
+                        }
+                        }
+                    </Formik>
+
+                    :
+
+                    <Formik
+                        initialValues={{
+                            code: ''
+                        }}
+                        validationSchema={Yup.object().shape({
+                            code: Yup.string()
+                                .required('이메일을 확인하여 코드를 입력해 주세요'),
+                        })}
+                        onSubmit={(values, { setSubmitting }) => {
+                            setTimeout(() => {
+
+                                let dataToSubmit = {
+                                    email : userEmail,
+                                    code : values.code
+                                }
+
+                                axios.post(`/api/checkNumber`, dataToSubmit)
+                                    .then(response => {
+                                        console.log(response.status)
+                                        if (response.status === 200) {
+                                            alert("변경할 비밀번호를 입력해 주세요")
+                                            settypeEmailCode(false)
+                                        }
+                                    })
+                                    .catch(err => {
+                                        alert("잘못된 인증번호입니다.")
+                                    })
+
+                                setSubmitting(false);
+                            }, 400);
+                        }}
+                    >
+                        {props => {
+                            const {
+                                values,
+                                touched,
+                                errors,
+                                isSubmitting,
+                                handleChange,
+                                handleBlur,
+                                handleSubmit,
+                            } = props
+                            return (
+                                <form onSubmit={handleSubmit}>
+                                    <Form.Item required>
+                                        <Input
+                                            id='code'
+                                            placeholder="인증번호"
+                                            type='code'
+                                            value={values.code}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            className={
+                                                errors.code && touched.code ? 'text-input error' : 'text-input'
+                                            }
+                                            size="large"
+                                        />
+                                        {errors.code && touched.code && (
+                                            <div className="input-feedback">{errors.code}</div>
+                                        )}
+                                    </Form.Item>
+                                    <Form.Item>
+                                        <Button type="primary" htmlType="submit" size="large" className="login-form-button" style={{ minWidth: '100%' }} disabled={isSubmitting} onSubmit={handleSubmit}>
+                                            확인
+                                    </Button>
+                                    </Form.Item>
+                                </form>
+                            )
+                        }
+                        }
+                    </Formik>
+
+                )
+
+            }
         </div>
     )
 }
