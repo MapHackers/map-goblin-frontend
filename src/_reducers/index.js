80e55598e3b189f0037c6cef41933fbf14679b76
SimHongSub <ghdtjq2038@gmail.com>
2021-05-09 18:05:24 +0900

    [mypage] conflict

--- src/_reducers/index.js
+++ src/_reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import user from './user_reducer'
 import map from './map_reducer'
+import alarm from "./alram_reducer";
 
 const rootReducer = combineReducers({
     user,
-    map
+    map,
+    alarm
 })
 
 export default rootReducer
--- src/redux/_actions/type.js
+++
@@ -1,4 +0,0 @@
-export const LOGIN_USER = "login_user"
-export const REGISTER_USER = "register_user"
-export const AUTH_USER = "auth_user"
-export const LOAD_ALARM = "load_alarm"
