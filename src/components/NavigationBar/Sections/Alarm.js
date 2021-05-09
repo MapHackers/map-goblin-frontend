9033e237ca1f7c084145d3b035985f1c5ac48f79
SimHongSub <ghdtjq2038@gmail.com>
2021-05-09 19:04:21 +0900

    [mypage] modify alarm

--- src/components/NavigationBar/Sections/Alarm.js
+++ src/components/NavigationBar/Sections/Alarm.js
@@ -2,81 +2,15 @@ import React, {useEffect, useState} from 'react'
 import { BellOutlined } from '@ant-design/icons';
 import styled from 'styled-components'
 import { Badge, Menu, Dropdown, Divider } from 'antd'
-import { connect } from 'react-redux'
-
+import {connect, useSelector} from 'react-redux'
 import Api from "../../../util/Api";
 
-function setRead(alarmData){
-    alarmData.read = true
-}
+function Alarm(props) {
 
-const mapStateToProps = state => ({
+    const alarms = useSelector(state => state.alarm.userAlarm.data)
 
-    userAlarm: state.user.userAlarm
-})
-
-function Alarm({userAlarm}, props) {
-
-    // const alarmDatas = [
-    //     {
-    //         id : 1,
-    //         user : 'dydfuf',
-    //         type : 'like',
-    //         read : true
-    //     },
-    //     {
-    //         id : 2,
-    //         user : 'oxhe2038',
-    //         type : 'clone',
-    //         read : true
-    //     },
-    //     {
-    //         id : 3,
-    //         user : 'unknown1',
-    //         type : 'issue',
-    //         read : true
-    //     },
-    //     {
-    //         id : 4,
-    //         user : 'ghdtjq',
-    //         type : 'request',
-    //         read : false
-    //     },
-    //     {
-    //         id : 5,
-    //         user : 'yongyeol',
-    //         type : 'like',
-    //         read : false
-    //     }
-    // ]
-
-    console.log(userAlarm)
-
-    const [unreadAlarmDatas, setUnreadAlarmDatas] = useState([userAlarm.data])
-    const tmpData = []
-    console.log("---------------------------",unreadAlarmDatas)
-
-    // useEffect(() => {
-    //     // console.log("Alarm.js ==============================",userAlarm.data)
-    //     // if(userAlarm !== undefined){
-    //     //     userAlarm.data.reverse().map(alarm => {
-    //     //         if (alarm.read === false){
-    //     //             tmpData.push(alarm)
-    //     //         }
-    //     //     })
-    //     //     setUnreadAlarmDatas(tmpData)
-    //     // }
-    //     if(userAlarm.data !== undefined){
-    //         userAlarm.data.map(alarm => {
-    //             if(alarm.read === false){
-    //                 tmpData.push(alarm)
-    //             }
-    //         })
-    //         setUnreadAlarmDatas(tmpData)
-    //     }
-    //
-    //
-    // }, [props])
+    console.log("//////////////////araaaa")
+    console.log(alarms)
 
     const typemap = {
         'LIKE' : '를 좋아합니다.',
@@ -84,18 +18,23 @@ function Alarm({userAlarm}, props) {
         'REQUEST' : '에 요청을 남겼습니다.',
         'ISSUE' : '에 이슈를 남겼습니다.'
     }
-    //console.log("alarma!!!!!",userAlarm.data)
-    // userAlarm.data.map(alarm => {
-    //     if(alarm.read === false){
-    //         tmpData.push(alarm)
-    //     }
-    // })
+
+    const onClickAlarm = (values) => {
+        console.log("values////////////////////")
+        console.log(values);
+
+        Api.post("/", {alarmId:values.key}).then(response => {
+            console.log(response);
+        }).catch(error => {
+            console.log(error);
+        })
+    }
 
     const AlarmList = (
         <Menu>
             {
-                [...userAlarm.data].map((alarm, idx) => (
-                    <Menu.Item key={idx} onClick={() => {}}>
+                alarms.map((alarm, idx) => (
+                    <Menu.Item key={alarm.id} onClick={onClickAlarm}>
                         <div>{alarm.dstMemberName}님이 회원님의 지도{typemap[alarm.alarmType]}</div>
                     </Menu.Item>
                 ))
@@ -107,8 +46,8 @@ function Alarm({userAlarm}, props) {
     return (
 
         <AlarmContainer>
-            <Badge count={userAlarm.data.length} size="small" >
-                <Dropdown overlay={AlarmList} trigger={['click']} onVisibleChange={() => { console.log(userAlarm.data)}} >
+            <Badge count={alarms.length} size="small" >
+                <Dropdown overlay={AlarmList} trigger={['click']} >
                     <BellOutlined style={{ fontSize: '1.5rem', marginTop: '0.1875rem' }} />
                 </Dropdown>
             </Badge>
@@ -117,7 +56,7 @@ function Alarm({userAlarm}, props) {
     )
 }
 
-export default connect(mapStateToProps)(Alarm)
+export default Alarm
 
 const AlarmContainer = styled.div`
     border-radius: 1.1rem;
