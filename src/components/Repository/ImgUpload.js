889684eafe6f5dc1c5a072d026e473f841d8ab36
SimHongSub <ghdtjq2038@gmail.com>
2021-05-16 02:26:32 +0900

    [repository] add image upload component

---
+++ src/components/Repository/ImgUpload.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Upload} from "antd";
+import ImgCrop from "antd-img-crop";
+import {useDispatch, useSelector} from "react-redux";
+import {withRouter} from "react-router-dom";
+import {addFile} from "../../_actions/repository_action";
+
+const ImgUpload = (props) => {
+    const dispatch = useDispatch();
+
+    const fileList = useSelector(state => state.repository.fileList)
+
+    const onChange = ({ fileList: newFileList }) => {
+        dispatch(addFile(newFileList));
+    };
+
+    return (
+        <ImgCrop rotate>
+            <Upload
+                listType="picture-card"
+                fileList={fileList}
+                onChange={onChange}
+                beforeUpload={() => {return false}}
+            >
+                {fileList.length < 1 && '+ Upload'}
+            </Upload>
+        </ImgCrop>
+    );
+};
+
+export default withRouter(ImgUpload);
