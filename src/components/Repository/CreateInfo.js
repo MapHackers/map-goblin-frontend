933ca9f57cce4e63bbb6425ff55990577438e5a1
SimHongSub <ghdtjq2038@gmail.com>
2021-05-16 20:33:56 +0900

    [repository] modify create info component

--- src/components/Repository/CreateInfo.js
+++ src/components/Repository/CreateInfo.js
@@ -1,9 +1,12 @@
-import React, {useState} from 'react';
-import {Button, Form, Input, Select, Tag} from "antd";
+import React, {useEffect, useState} from 'react';
+import {Button, Col, Form, Input, Select, Tag} from "antd";
 import ImgUpload from "./ImgUpload";
 import {withRouter} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
-import {fileUpload, saveRepositoryInfo} from "../../_actions/repository_action";
+import {addFile, fileUpload, saveRepositoryInfo} from "../../_actions/repository_action";
+import CreateForm from "./CreateForm";
+import CreateFormItem from "./CreateFormItem";
+import SelectCategory from "./SelectCategory";
 
 const { TextArea } = Input;
 
@@ -25,7 +28,7 @@ const tailFormItemLayout = {
             offset: 10,
         },
         sm: {
-            span: 10,
+            span: 15,
             offset: 9,
         },
     },
@@ -33,119 +36,151 @@ const tailFormItemLayout = {
 
 const CreateInfo = (props) => {
 
-    console.log("&&&&&&&&&&&&&&&&&&&&&&&&")
-    console.log(props.repositoryInfo)
-    console.log(props.thumbnailUrl)
-
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
     const user = useSelector(state => state.user);
     const repository = useSelector(state => state.repository);
 
-    const onFinish = (values) => {
+    useEffect(()=>{
+        let newFileList = [];
 
-        const formData = new FormData();
+        dispatch(addFile(newFileList));
+    }, [])
 
-        if (repository.fileList.length > 0) {
-            formData.append('file', repository.fileList[0].originFileObj);
-            dispatch(fileUpload(formData))
-                .then(response => {
-                    const payload = response.payload;
-
-                    values.thumbnail = payload.data;
-                    dispatch(saveRepositoryInfo(values))
-                        .then(response => {
-                            const payload = response.payload;
-
-                            props.history.push(`/${user.userId}/repositories/${payload.data.name}`);
-                        })
-                });
-        }else{
-            dispatch(saveRepositoryInfo(values))
-                .then(response => {
-                    props.history.push(`/${user.userId}/repositories/${response.data.name}`);
-                });
-        }
-    };
-
-    const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
-    };
-
-    const options = [{ value: '대학교' }, { value: '맛집' }, { value: '정보전달' }, { value: '서울' }];
-
-    const tagRender = (props) => {
-        const { label, closable, onClose } = props;
-        const onPreventMouseDown = event => {
-            event.preventDefault();
-            event.stopPropagation();
-        };
-        return (
-            <Tag
-                color='geekblue'
-                onMouseDown={onPreventMouseDown}
-                closable={closable}
-                onClose={onClose}
-                style={{ marginRight: 3 }}
-            >
-                {label}
-            </Tag>
-        );
-    };
+    // console.log("&&&&&&&&&&&&&&&&&&&&&&&&")
+    // console.log(props.repositoryInfo)
+    // console.log(props.thumbnailUrl)
+    //
+    // const dispatch = useDispatch()
+    //
+    // const user = useSelector(state => state.user);
+    // const repository = useSelector(state => state.repository);
+    //
+    // const onFinish = (values) => {
+    //
+    //     const formData = new FormData();
+    //
+    //     if (repository.fileList.length > 0) {
+    //         formData.append('file', repository.fileList[0].originFileObj);
+    //         dispatch(fileUpload(formData))
+    //             .then(response => {
+    //                 const payload = response.payload;
+    //
+    //                 values.thumbnail = payload.data;
+    //                 dispatch(saveRepositoryInfo(values))
+    //                     .then(response => {
+    //                         const payload = response.payload;
+    //
+    //                         props.history.push(`/${user.userId}/repositories/${payload.data.name}`);
+    //                     })
+    //             });
+    //     }else{
+    //         dispatch(saveRepositoryInfo(values))
+    //             .then(response => {
+    //                 props.history.push(`/${user.userId}/repositories/${response.data.name}`);
+    //             });
+    //     }
+    // };
+    //
+    // const onFinishFailed = (errorInfo) => {
+    //     console.log('Failed:', errorInfo);
+    // };
+    //
+    // const options = [{ value: '대학교' }, { value: '맛집' }, { value: '정보전달' }, { value: '서울' }];
+    //
+    // const tagRender = (props) => {
+    //     const { label, closable, onClose } = props;
+    //     const onPreventMouseDown = event => {
+    //         event.preventDefault();
+    //         event.stopPropagation();
+    //     };
+    //     return (
+    //         <Tag
+    //             color='geekblue'
+    //             onMouseDown={onPreventMouseDown}
+    //             closable={closable}
+    //             onClose={onClose}
+    //             style={{ marginRight: 3 }}
+    //         >
+    //             {label}
+    //         </Tag>
+    //     );
+    // };
 
     return (
-        <Form {...formItemLayout}
-              name="basic"
-              onFinish={onFinish}
-              onFinishFailed={onFinishFailed}
-              initialValues={{ remember: true }}>
-
-            <Form.Item
-                label="썸네일"
-                name="thumbnail"
-                style={{width:"50%", marginLeft:"25%"}}>
+        <CreateForm formName="create" repository={repository} user={user}>
+            <CreateFormItem label="썸네일" name="thumbnail" style={{width:"50%", marginLeft:"25%"}}>
                 <div id="create-map-upload">
-                    <ImgUpload thumbnailUrl={props.thumbnailUrl}/>
+                    <ImgUpload />
                 </div>
-            </Form.Item>
-
-            <Form.Item
-                label="지도 이름"
-                name="name"
-                rules={[{ required: true, message: '지도 이름을 입력해주세요!' }]}
-                style={{width:"50%", marginLeft:"25%"}}
-            >
+            </CreateFormItem>
+            <CreateFormItem label="지도 이름" name="name" rules={[{ required: true, message: '지도 이름을 입력해주세요!' }]} style={{width:"50%", marginLeft:"25%"}}>
                 <Input />
-            </Form.Item>
-
-            <Form.Item
-                label="설명"
-                name="description"
-                style={{width:"50%", marginLeft:"25%"}}
-            >
-                <TextArea />
-            </Form.Item>
-
-            <Form.Item
-                label="카테고리"
-                name="categories"
-                style={{width:"50%", marginLeft:"25%"}}
-            >
-                <Select
-                    mode="multiple"
-                    showArrow
-                    tagRender={tagRender}
-                    style={{ width: '100%' }}
-                    options={options}
-                />
-            </Form.Item>
-
-            <Form.Item {...tailFormItemLayout}>
+            </CreateFormItem>
+            <CreateFormItem label="설명" name="description" style={{width:"50%", marginLeft:"25%"}}>
+                <TextArea rows={5}/>
+            </CreateFormItem>
+            <CreateFormItem label="카테고리" name="categories" style={{width:"50%", marginLeft:"25%"}}>
+                <SelectCategory />
+            </CreateFormItem>
+            <CreateFormItem wrapperCol={tailFormItemLayout}>
                 <Button type="primary" htmlType="submit">
                     지도 생성
                 </Button>
-            </Form.Item>
-        </Form>
+            </CreateFormItem>
+        </CreateForm>
+        // <Form {...formItemLayout}
+        //       name="basic"
+        //       onFinish={onFinish}
+        //       onFinishFailed={onFinishFailed}
+        //       initialValues={{ remember: true }}>
+        //
+        //     <Form.Item
+        //         label="썸네일"
+        //         name="thumbnail"
+        //         style={{width:"50%", marginLeft:"25%"}}>
+        //         <div id="create-map-upload">
+        //             <ImgUpload thumbnailUrl={props.thumbnailUrl}/>
+        //         </div>
+        //     </Form.Item>
+        //
+        //     <Form.Item
+        //         label="지도 이름"
+        //         name="name"
+        //         rules={[{ required: true, message: '지도 이름을 입력해주세요!' }]}
+        //         style={{width:"50%", marginLeft:"25%"}}
+        //     >
+        //         <Input />
+        //     </Form.Item>
+        //
+        //     <Form.Item
+        //         label="설명"
+        //         name="description"
+        //         style={{width:"50%", marginLeft:"25%"}}
+        //     >
+        //         <TextArea />
+        //     </Form.Item>
+        //
+        //     <Form.Item
+        //         label="카테고리"
+        //         name="categories"
+        //         style={{width:"50%", marginLeft:"25%"}}
+        //     >
+        //         <Select
+        //             mode="multiple"
+        //             showArrow
+        //             tagRender={tagRender}
+        //             style={{ width: '100%' }}
+        //             options={options}
+        //         />
+        //     </Form.Item>
+        //
+        //     <Form.Item {...tailFormItemLayout}>
+        //         <Button type="primary" htmlType="submit">
+        //             지도 생성
+        //         </Button>
+        //     </Form.Item>
+        // </Form>
     );
 };
 
