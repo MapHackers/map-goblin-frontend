68e3a2d89a041fd4fa7a23144806b72d54edbd00
SimHongSub <ghdtjq2038@gmail.com>
2021-05-20 21:58:48 +0900

    [repository] add CreateRequestPage

---
+++ src/pages/CreateRequestPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateRequestPage = () => {
+    return (
+        <div>
+            request create page
+        </div>
+    );
+};
+
+export default CreateRequestPage;
